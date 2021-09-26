264f50ddd9f19f083060d10bd775b678052c9f83
joshkent94 <josh.kent94@yahoo.co.uk>
2021-09-26 22:11:45 +0100

    Add session table creation to init.sql

--- server/init.sql
+++ server/init.sql
@@ -13,3 +13,14 @@ CREATE TABLE genres (
     user_id INTEGER NOT NULL REFERENCES users ON DELETE CASCADE,
     genres VARCHAR(255) ARRAY
 );
+
+CREATE TABLE "session" (
+  "sid" varchar NOT NULL COLLATE "default",
+	"sess" json NOT NULL,
+	"expire" timestamp(6) NOT NULL
+)
+WITH (OIDS=FALSE);
+
+ALTER TABLE "session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
+
+CREATE INDEX "IDX_session_expire" ON "session" ("expire");
