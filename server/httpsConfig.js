2d4e4dd7fcb52d285bd446d0f202f1120e5bda5a
joshkent94 <josh.kent94@yahoo.co.uk>
2021-09-26 23:58:25 +0100

    Removing SSL certificates

--- server/httpsConfig.js
+++ server/httpsConfig.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 
 // https credentials config
-const privateKey = fs.readFileSync('key.pem', 'utf8');
-const certificate = fs.readFileSync('cert.pem', 'utf8');
+const privateKey = fs.readFileSync('/Users/josh.kent/key.pem', 'utf8');
+const certificate = fs.readFileSync('/Users/josh.kent/cert.pem', 'utf8');
 const credentials = {
     key: privateKey,
     cert: certificate
