18b1d651f47c639221eabc7beece2e4fd9ee8a44
joshkent94 <josh.kent94@yahoo.co.uk>
2021-09-27 00:09:10 +0100

    Production SSL attempt

--- server/httpsConfig.js
+++ server/httpsConfig.js
@@ -1,8 +1,10 @@
 const fs = require('fs');
+const { isProduction } = require('./connectionConfig');
 
 // https credentials config
-const privateKey = fs.readFileSync('/Users/josh.kent/key.pem', 'utf8');
-const certificate = fs.readFileSync('/Users/josh.kent/cert.pem', 'utf8');
+const privateKey = fs.readFileSync(isProduction ? '../suadkey.pem' : '/Users/josh.kent/key.pem', 'utf8');
+const certificate = fs.readFileSync(isProduction ? '../suadcert.pem' : '/Users/josh.kent/cert.pem', 'utf8');
+
 const credentials = {
     key: privateKey,
     cert: certificate
