950d04c0cb10d42504a3099787417f31bdea60a3
joshkent94 <josh.kent94@yahoo.co.uk>
2021-09-26 21:48:22 +0100

    Amending SSL config

--- server/connectionConfig.js
+++ server/connectionConfig.js
@@ -7,7 +7,7 @@ const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_P
 
 const pool = new Pool({
     connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-    ssl: isProduction,
+    ssl: isProduction ? { rejectUnauthorized: false } : false
 });
 
 module.exports = { pool, isProduction };
