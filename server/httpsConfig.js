df08c1e4f8de6a83ad5e7ebfd0bebc466e561977
joshkent94 <josh.kent94@yahoo.co.uk>
2021-09-27 00:12:18 +0100

    Another SSL attempt

--- server/httpsConfig.js
+++ server/httpsConfig.js
@@ -2,8 +2,8 @@ const fs = require('fs');
 const { isProduction } = require('./connectionConfig');
 
 // https credentials config
-const privateKey = fs.readFileSync(isProduction ? '../suadkey.pem' : '/Users/josh.kent/key.pem', 'utf8');
-const certificate = fs.readFileSync(isProduction ? '../suadcert.pem' : '/Users/josh.kent/cert.pem', 'utf8');
+const privateKey = fs.readFileSync(isProduction ? '/suadkey.pem' : '/Users/josh.kent/key.pem', 'utf8');
+const certificate = fs.readFileSync(isProduction ? '/suadcert.pem' : '/Users/josh.kent/cert.pem', 'utf8');
 
 const credentials = {
     key: privateKey,
