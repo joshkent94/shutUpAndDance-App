0dd842a4ad8bc771744eb186a9a627dc749fbb53
joshkent94 <josh.kent94@yahoo.co.uk>
2021-09-26 23:39:05 +0100

    Changing SSL

--- server.js
+++ server.js
@@ -62,7 +62,6 @@ app.get('*', (req, res) => {
 });
 
 // run https server
-console.log(credentials);
 const httpsServer = https.createServer(credentials, app);
 httpsServer.listen(port, () => {
     console.log(`App is running on port ${port}.`);
--- server/httpsConfig.js
+++ server/httpsConfig.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 // https credentials config
-const privateKey = fs.readFileSync('~key.pem', 'utf8');
+const privateKey = fs.readFileSync('key.pem', 'utf8');
 const certificate = fs.readFileSync('cert.pem', 'utf8');
 const credentials = {
     key: privateKey,
