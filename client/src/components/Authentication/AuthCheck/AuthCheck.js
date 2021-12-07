04be060f6e161f82d744a9a3e10a13ae56bfaf64
joshkent94 <josh.kent94@yahoo.co.uk>
2021-12-07 22:15:30 +0000

    Attempted fix for auth

--- client/src/components/Authentication/AuthCheck/AuthCheck.js
+++ client/src/components/Authentication/AuthCheck/AuthCheck.js
@@ -2,6 +2,7 @@ import { Navigate, useLocation } from "react-router-dom";
 
 export default function AuthCheck({ children }) {
     const location = useLocation();
+    console.log(location.pathname);
 
     let content;
     if (location.pathname === '/login') {
