216ddf01d27fc0b13022f2da85afa83615f3b395
joshkent94 <josh.kent94@yahoo.co.uk>
2021-12-26 00:38:28 +0000

    Remove redundant song suggestions component

--- client/src/components/SuggestionsFeature/Suggestions/Suggestions.js
+++ client/src/components/SuggestionsFeature/Suggestions/Suggestions.js
@@ -1,14 +1,18 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import GenreDropdown from '../GenreDropdown/GenreDropdown';
-import { getAvailableGenres, selectAccessToken, selectAvailableGenres } from "../../../utils/state/suggestionsSlice";
-import SongSuggestions from "../SongSuggestions/SongSuggestions";
+import { getAvailableGenres, getSuggestions, selectAccessToken, selectAvailableGenres, selectSuggestions } from "../../../utils/state/suggestionsSlice";
+import { selectGenres } from "../../../utils/state/userSlice";
+import Suggestion from "../Suggestion/Suggestion";
 import './Suggestions.css';
 
 export default function Suggestions() {
     const availableGenres = useSelector(selectAvailableGenres);
     const accessToken = useSelector(selectAccessToken);
+    const suggestions = useSelector(selectSuggestions);
+    const genres = useSelector(selectGenres);
     const dispatch = useDispatch();
+    const firstRender = useRef(true);
 
     // get genre list from Spotify
     useEffect(() => {
@@ -19,6 +23,32 @@ export default function Suggestions() {
         };
     }, [dispatch, accessToken, availableGenres]);
 
+    // get song suggestions from Spotify on initial page load
+    useEffect(() => {
+        if (firstRender.current) {
+            firstRender.current = false;
+            if (suggestions.length === 0 && document.cookie && accessToken !== '') {
+                dispatch(getSuggestions({
+                    accessToken: accessToken,
+                    genres: genres
+                }));
+            };
+        };
+    });
+
+    let content;
+    if (suggestions.length === 0) {
+        content =
+            <h5 className="sub-heading">
+                Please select at least one genre to see suggestions.
+            </h5>;
+    } else {
+        content =
+            suggestions.map(track => {
+                return <Suggestion key={track.id} track={track} />
+            });
+    };
+
     return (
         <div className="page">
             <div className="page-header">
@@ -28,7 +58,9 @@ export default function Suggestions() {
                 <GenreDropdown />
             </div>
             <div className="page-content">
-                <SongSuggestions />
+                <div id="suggestions-page">
+                    {content}
+                </div>
             </div>
         </div>
     );
