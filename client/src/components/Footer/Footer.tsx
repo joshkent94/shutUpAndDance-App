d5d33b855d9bff76741d3e6d9e1f6218dfd45a29
joshkent94 <josh.kent94@yahoo.co.uk>
2022-02-25 00:29:07 +0000

    Start conversion to less.css

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -1,11 +1,15 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
 import { faCopyright, faHeart } from '@fortawesome/free-solid-svg-icons';
-import './Footer.css';
+import './Footer.less';
 
 export default function Footer() {
+    const faCopyrightProp = faCopyright as IconProp;
+    const faHeartProp = faHeart as IconProp;
+
     return (
         <div id="footer">
-            <p id="footer-message">Made with <FontAwesomeIcon icon={faHeart} /> by Shut Up And Dance Ltd. <FontAwesomeIcon icon={faCopyright} /> All rights reserved.</p>
+            <p id="footer-message">Made with <FontAwesomeIcon icon={faHeartProp} /> by Shut Up And Dance Ltd. <FontAwesomeIcon icon={faCopyrightProp} /> All rights reserved.</p>
         </div>
     );
 };
