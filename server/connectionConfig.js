6a4a1a6a387b7de27f42eea1b9ba42139f0017fa
Josh Kent <josh.kent@pendo.io>
2023-04-07 17:28:19 +0100

    New config

--- server/connectionConfig.js
+++ server/connectionConfig.js
@@ -6,7 +6,7 @@ const { Pool } = require('pg');
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 
 const pool = new Pool({
-    connectionString: isProduction ? process.env.HEROKU_POSTGRESQL_COPPER_URL : connectionString,
+    connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
     ssl: isProduction ? { rejectUnauthorized: false } : false
 });
 
